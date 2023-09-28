c768baf3ff2139212c5e076e55cfd7b240aee7a7
Shailja Gupta <115636055+shailja026@users.noreply.github.com>
2023-09-28 19:40:05 +0530

    some designs were added

--- server/databse/db.js
+++ server/databse/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async (username = 'shailjagupta', password = 'shailjagupta') => {
-    const URL = `mongodb+srv://${username}:${password}@cluster0.kyljx0t.mongodb.net/?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://shailjagupta4466:shailja123@new.3r2loco.mongodb.net/?retryWrites=true&w=majority`
 
     try {
         await mongoose.connect(URL , {useUnifiedTopology : true , useNewUrlParser : true})
